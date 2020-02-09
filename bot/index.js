9a41aa839c28cb7b645e29da4c461eee61aeee03
NCJ <iamncj@outlook.com>
2020-02-09 10:24:36 +0800

    not elegant

--- bot/index.js
+++ bot/index.js
@@ -9,12 +9,25 @@ const app = new App({
   commandPrefix: '.'
 })
 
+var responceData = {}
+
+function init() {
+  var reader = new FileReader();
+  try {
+    reader.readAsText('./data.json')
+    responceData = JSON.parse(reader.result)
+  } catch {
+    console.log('Error occured when reading data')
+    responceData = {}
+  }
+}
+
+init()
+
 app.start()
 
 app.command('echo <message>')
   .action(({ meta }, message) => meta.$send(message))
-
-var responceData = {}
 
 // add command
 app.command('add <keyword> <responce>')
@@ -96,6 +109,38 @@ app.middleware((meta, next) => {
 
 // repeater
 // TODO bug: fix repeat for different group
+// let times = 0 // 已复读次数
+// let message = '' // 当前信息
+
+// app.middleware((meta, next) => {
+//   var repeatThreshold = 2        // 触发次数
+//   var atThreshold = 3            // Q人次数
+//   var interruptThreshold = 6     // 打破次数
+//   var interruptThresholdPlus = 8 // 超级Q人
+//   if (meta.messageType === 'group' && meta.message === message) { 
+//     // repeater only works for group message
+//     times += 1
+//     // console.log(times)
+//     // console.log(meta.userId)
+//     if (times === repeatThreshold) return meta.$send(message)
+//     else if (times == interruptThreshold) {
+//       return meta.$send('打断复读')
+//     }
+//     else if (times == interruptThresholdPlus) {
+//       return meta.$send(`[CQ:at,qq=${meta.userId}] 在？为什么还在复读？`)
+//     }
+//   } else {
+//     let lastTimes = times
+//     times = 1
+//     message = meta.message
+//     if (lastTimes > atThreshold) {
+//       return meta.$send(`[CQ:at,qq=${meta.userId}] 在？为什么打断复读？就这么对待群友的劳动成果？`)
+//     }
+//     return next()
+//   }
+// })
+
+
 let times = 0 // 已复读次数
 let message = '' // 当前信息
 
